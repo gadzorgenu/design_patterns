7c9177a6e96be863c254c0df1f0d620f11cbb320
Georgina Adzorgenu <gadzorgenu@gmail.com>
2023-06-27 23:12:07 +0000

    Updated Readme

---
+++ src/main/java/com/design_patterns/design_pattern/Singleton_design_pattern/GetTiles.java
@@ -0,0 +1,21 @@
+package com.design_patterns.design_pattern.Singleton_design_pattern;
+
+import java.util.LinkedList;
+
+public class GetTiles implements  Runnable{
+    @Override
+    public void run() {
+        // How you create a new instance of Singleton
+        Singleton newInstance = Singleton.getInstance();
+
+        // Get unique id for instance object
+        System.out.println("1st Instance ID: " + System.identityHashCode(newInstance));
+        // Get all of the letters stored in the List
+
+        System.out.println(newInstance.getLettersList());
+        LinkedList<String> playerOneTiles = newInstance.getTiles(7);
+
+        System.out.println("Player 1: " + playerOneTiles);
+        System.out.println("Got Tiles");
+    }
+}
